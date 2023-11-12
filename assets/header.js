375d32fce53c813a96cce8cf97e122a30c8948ca
Pixiemay <kondakov2007.andrey@gmail.com>
2023-11-12 10:57:02 +0700

    Сделал скрипт на скрытие header, добавлен текст в начале страницы

--- assets/header.js
+++ assets/header.js
@@ -1,31 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => { // DOM готов к взаимодейтсвию
+let prevScrollPos = window.pageYOffset;
+    const header = document.getElementById("mainHeader");
 
-    const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
-  
-      const header = document.querySelector('.header') // находим header и записываем в константу
-  
-      let prevScroll = window.pageYOffset // узнаем на сколько была прокручена страница ранее
-      let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
-  
-      window.addEventListener('scroll', () => { // при прокрутке страницы
-  
-        currentScroll = window.pageYOffset // узнаем на сколько прокрутили страницу
-  
-        const headerHidden = () => header.classList.contains('header_hidden') // узнаем скрыт header или нет
-  
-        if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
-          header.classList.add('header_hidden') // то скрываем header
-        }
-        if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
-          header.classList.remove('header_hidden') // то отображаем header
-        }
-  
-        prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
-  
-      })
-  
+    window.onscroll = function() {
+      const currentScrollPos = window.pageYOffset;
+
+      if (prevScrollPos > currentScrollPos) {
+        header.classList.remove("header-hidden");
+      } else {
+        header.classList.add("header-hidden");
+      }
+
+      prevScrollPos = currentScrollPos;
     }
-  
-    onScrollHeader() // вызываем основную функцию onScrollHeader
-  
-  });  
